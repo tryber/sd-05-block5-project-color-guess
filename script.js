8e32dd99a6fd1135c6d46e2daba34e697ff89d5c
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-05-25 12:12:55 -0300

    Codeclimate attempt

--- script.js
+++ script.js
@@ -1,35 +1,37 @@
 const numColors = 6;
 let colors = [];
 let selectedColor = {};
-function createRandomColors() {
-  let r = Math.round(Math.random()*255);
-  let g = Math.round(Math.random()*255);
-  let b = Math.round(Math.random()*255);
-  return [r ,g ,b];
+function createRandomColors () {
+  const r = Math.round(Math.random() * 255);
+  const g = Math.round(Math.random() * 255);
+  const b = Math.round(Math.random() * 255);
+  return [r, g, b];
 }
-function selectRandomColor(){
+function selectRandomColor () {
   const size = colors.children.length;
   const c = colors.children[Math.floor(Math.random()*size)].color;
   selectedColor.color = c;
-  selectedColor.innerText = '('+c[0]+', '+c[1]+', '+c[2]+')';
+  selectedColor.innerText = '(' + String(c[0]) + ', ' + String(c[1]) + ', ' + String(c[2]) + ')';
+  document.getElementById('answer').innerText = 'Escolha uma cor';
 }
 window.onload = function () {
   colors = this.document.getElementById('colors');
   selectedColor = this.document.getElementById('rgb-color');
-  while (colors.children.length < numColors){
+  function gamble(element) {
+    if(element.target.color === selectedColor.color) {
+      document.getElementById('answer').innerText = 'Acertou!';
+    } else {
+      document.getElementById('answer').innerText = 'Errou! Tente novamente!';
+    }
+  }
+  while (colors.children.length < numColors) {
     const div = this.document.createElement('div');
     const child = colors.appendChild(div);
     const newColor = createRandomColors();
     child.classList.add('ball');
     child.color = newColor;
-    child.style.backgroundColor = 'RGB('+newColor[0]+','+newColor[1]+','+newColor[2]+')';
-    child.addEventListener('click', function(element) {
-      if(element.target.color === selectedColor.color) {
-        document.getElementById('answer').innerText = "Acertou!";
-      } else {
-        document.getElementById('answer').innerText = "Errou! Tente novamente!";
-      }
-    });
+    child.style.backgroundColor = 'RGB(' + String(newColor[0]) + ',' + String(newColor[1]) + ',' + String(newColor[2]) + ')';
+    child.addEventListener('click', gamble);
   }
   selectRandomColor();
 };
