09b3417ac2f63e35d4b238702a19483334ca7488
pauloricardoz <trybe.przt@gmail.com>
2020-05-25 16:57:46 -0300

    RGB id correct3

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let cor_sorteada = []
 gerar_RGB();
 function gerar_RGB() {
   cor_sorteada = [cor(), cor(), cor()];
-  document.getElementById('cor_RGB').innerHTML='('+cor_sorteada.join(',')+')'
+  document.getElementById('rgb-color').innerHTML= "("+cor_sorteada.join(',')+")"
 }
 function cor() {
   return Math.floor(Math.random() * 255);
