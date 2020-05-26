d242e6243eef3eb6807d33c821a004f725c3fd12
rfreitasbatista <rfreitasbatista@gmail.com>
2020-05-26 16:49:58 -0300

    Esqueci dos commits

---
+++ script.js
@@ -0,0 +1,38 @@
+const rgbcolor = document.getElementById('rgb-color');
+const answer = document.getElementById('answer');
+const ballColors = document.getElementsByClassName('ball')
+window.onload = function () {
+let random = Math.floor(Math.random() * 6)
+ballColors[0].style.backgroundColor = 'rgb'+geraRGB();
+ballColors[1].style.backgroundColor = 'rgb'+geraRGB();
+ballColors[2].style.backgroundColor = 'rgb'+geraRGB();
+ballColors[3].style.backgroundColor = 'rgb'+geraRGB();
+ballColors[4].style.backgroundColor = 'rgb'+geraRGB();
+ballColors[5].style.backgroundColor = 'rgb'+geraRGB();
+rgbcolor.innerHTML = ballColors[random].style.backgroundColor.replace('rgb', '');
+}
+//FUNÇAO QUE GERA AS CORES
+function geraRGB () {
+  let R = Math.floor(Math.random() * 256);
+  let G = Math.floor(Math.random() * 256);
+  let B = Math.floor(Math.random() * 256);
+  let cor = '('+R+', '+G+', '+B+')'
+  return cor;
+}
+
+//VERIFICA SE ESTA TUDO CERTO OU ERRADO
+function certoOuErrado (event){
+  let target = event.target
+  let targetColor = target.style.backgroundColor
+  if (targetColor === 'rgb'+rgbcolor.innerHTML) {
+    answer.innerHTML = 'Acertou!'
+  } else {
+    answer.innerHTML = 'Errou! Tente novamente!'
+  }
+}
+ballColors[0].addEventListener('click', certoOuErrado);
+ballColors[1].addEventListener('click', certoOuErrado);
+ballColors[2].addEventListener('click', certoOuErrado);
+ballColors[3].addEventListener('click', certoOuErrado);
+ballColors[4].addEventListener('click', certoOuErrado);
+ballColors[5].addEventListener('click', certoOuErrado);
