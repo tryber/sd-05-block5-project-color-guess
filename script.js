5be238f3e7b72157c63c7f08dcb34354a6c831a2
pedrohcalado <pedrocalado22@gmail.com>
2020-05-27 16:38:41 -0300

    Add random colors to the balls

--- script.js
+++ script.js
@@ -6,5 +6,18 @@ function randomColor () {
   return `(${r}, ${g}, ${b})`;
 }
 
-// add to the p element the result of the randomColor function
-document.querySelector('p').innerHTML = randomColor();
+// add event listener to check whether the color of the clicked button matches the rgb color
+const balls = document.querySelector('.ball');
+balls.addEventListener('click', function (event) {
+  let colorSelected = event.target.style.backgroundColor;
+});
+
+// add random colors to the balls
+const buttons = document.querySelectorAll('button');
+for (let i = 0; i < buttons.length; i += 1) {
+  buttons[i].style.backgroundColor = 'rgb' + randomColor();
+}
+
+// add to the p element the background color of one of the balls
+let randomInteger = Math.floor(Math.random() * 6 + 1);
+document.querySelector('.rgb-color').innerHTML = buttons[randomInteger].style.backgroundColor.replace('rgb', '');
