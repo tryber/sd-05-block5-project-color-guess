681acb04daf48b364f903075e1cd18471b02f4e2
pedrohcalado <pedrocalado22@gmail.com>
2020-05-27 19:36:05 -0300

    Add score

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
 // function that creates a random rgb color
-function randomColor () {
+function randomColor() {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
   return `(${r}, ${g}, ${b})`;
 }
 
+// add to the p.rgb-color element the background color of one of the balls
+let randomInteger = Math.floor((Math.random() * 5)  + 1);
+const mainColor = document.querySelector('.rgb-color');
+mainColor.innerHTML = buttons[randomInteger].style.backgroundColor.replace('rgb', '');
+
 // add event listener to check whether the color of the clicked button matches the rgb color
 const balls = document.querySelector('.balls');
 let score = 0;
-localStorage.setItem('score', score);
-balls.addEventListener('click', function (event) {
-  let colorSelected = event.target.style.backgroundColor.replace('rgb', '');
-  if (mainColor.innerHTML === colorSelected) {
-    document.querySelector('.answer').innerHTML = 'Acertou!';
-    // count the matches and add it to local storage
-    score += 3;
-    document.querySelector('span').innerHTML = localStorage.getItem('score');
-  } else {
-    document.querySelector('.answer').innerHTML = 'Errou! Tente novamente!';
-  }
-  
-  document.querySelector('span').innerHTML = localStorage.getItem('score');
-});
 
 window.onload = function () {
-  score = localStorage.getItem('score');
+  document.querySelector('span').innerHTML = localStorage.getItem('score');
+  balls.addEventListener('click', function (event) {
+    const colorSelected = event.target.style.backgroundColor.replace('rgb', '');
+    if (mainColor.innerHTML === colorSelected) {
+      document.querySelector('.answer').innerHTML = 'Acertou!';
+      // count the matches and add it to local storage
+      score = parseInt(localStorage.getItem('score')) + 3;
+      localStorage.setItem('score', score);
+      document.querySelector('span').innerHTML = localStorage.getItem('score');
+    } else {
+      document.querySelector('.answer').innerHTML = 'Errou! Tente novamente!';
+    }
+  });
+  
 }
 
 // add random colors to the balls
@@ -33,11 +37,6 @@ const buttons = document.getElementsByClassName('ball');
 for (let i = 0; i < buttons.length; i += 1) {
   buttons[i].style.backgroundColor = 'rgb' + randomColor();
 }
-
-// add to the p.rgb-color element the background color of one of the balls
-let randomInteger = Math.floor(Math.random() * 5 + 1);
-const mainColor = document.querySelector('.rgb-color');
-mainColor.innerHTML = buttons[randomInteger].style.backgroundColor.replace('rgb', '');
 
 // button that reload the game
 const reloadButton = document.querySelector('.start-again');
