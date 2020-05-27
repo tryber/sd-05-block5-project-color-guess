0f9e82c9b7799f8440ebfd315e41167b0811029c
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-05-26 21:30:45 -0300

    adicionado função para resetar game e se o Usuário acertou

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 let randomText = document.getElementById('rgb-color');
+let balls = document.getElementsByClassName('ball');
+let ballsLabel = document.getElementById('ball-content');
+let resetButton = document.getElementById('reset-button');
+let answer = document.getElementById('answer');
+let target = '';
 
 function randomColor () {
   let r = Math.floor(Math.random()* 255);
@@ -12,5 +17,33 @@ function randomTextColor() {
   randomText.innerHTML = `(${randomColor()})`;
 }
 
+function randomBallsColor(){
+  let random = Math.floor(Math.random() * 5.9999999);
+  for( let i = 0; i < balls.length; i += 1) {
+    balls[i].style.backgroundColor = `rgb(${randomColor()})`;    
+  }
+  randomText.innerHTML = balls[random].style.backgroundColor.replace('rgb', '');
+  answer.innerHTML = '';
+}
+
+function answerText() {
+  target = event.target;
+  let word = 'rgb'+randomText.innerHTML;
+  console.log(word);
+
+  if(target.style.backgroundColor === 'rgb' + randomText.innerHTML ){
+    answer.innerHTML = 'Acertou!'
+  } else {
+    answer.innerHTML = 'Errou! Tente novamente!';
+  }
+}
+
+function resetGame() {
+
+}
+
+ballsLabel.addEventListener('click', answerText);
+resetButton.addEventListener('click', randomBallsColor);
 window.addEventListener('load', randomTextColor());
+window.addEventListener('load', randomBallsColor());
 
