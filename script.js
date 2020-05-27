c657994e0c7b59d7b01944ae5c7ac14878fad8f2
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-05-27 12:43:51 -0300

    quick fix to reset button

--- script.js
+++ script.js
@@ -1,25 +1,10 @@
-{
-  /* <h3 id="rgb-color"></h3>
-    <div class="ball"></div>
-    <div class="ball"></div>
-    <div class="ball"></div>
-    <div class="ball"></div>
-    <div class="ball"></div>
-    <div class="ball"></div>
-    <h3 id="answer">Escolha uma cor</h3>
-    <button id="reset">Reiniciar Jogo</button>
-    <button id="score"></button> */
-}
 const rgbColor = document.getElementById('rgb-color');
 const ballsContainer = document.getElementsByClassName('balls-container')[0]
   .children;
 const answer = document.getElementById('answer');
-const reset = document.getElementById('reset');
+const reset = document.getElementById('reset-game');
 const score = document.getElementById('score');
 let pontos = 0;
-
-reset.addEventListener('click',resetColors )
-window.onload = resetColors()
 
 function compareBallWithChosenRGB(event) {
   let colorOfClicked = event.target.style.backgroundColor;
@@ -42,19 +27,14 @@ function getRandomRGB() {
 }
 
 function resetColors() {
-for (let i = 0; i < ballsContainer.length; i++) {
-  ballsContainer[i].style.backgroundColor = getRandomRGB();
+  for (let i = 0; i < ballsContainer.length; i++) {
+    ballsContainer[i].style.backgroundColor = getRandomRGB();
+  }
+  let prizedBall;
+  selectPrizeBall();
+  getInnerRGB();
+  rgbColor.innerHTML = getInnerRGB();
 }
-let prizedBall;
-selectPrizeBall()
-getInnerRGB()
-rgbColor.innerHTML = getInnerRGB();
-
-}
-
-// //Variável que guarda a bola que será a ganhadora
-// let prizedBall;
-
 function selectPrizeBall() {
   let prizedBallPosition = Math.floor(Math.random() * 6);
   prizedBall = ballsContainer[prizedBallPosition];
@@ -63,9 +43,8 @@ function selectPrizeBall() {
 
 function getInnerRGB() {
   let innerRGB = prizedBall.style.backgroundColor;
-  
   return innerRGB;
 }
 
-
-
+reset.addEventListener('click', resetColors);
+window.onload = resetColors();
