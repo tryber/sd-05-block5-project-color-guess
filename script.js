bb464c846fbd851a5979a931a4367505039c339f
pauloricardoz <trybe.przt@gmail.com>
2020-05-28 16:33:44 -0300

    button id

--- script.js
+++ script.js
@@ -9,8 +9,6 @@ function cor() {
   let sort = Math.floor(Math.random() * 255);
   return sort > 10 ? sort : 10;
 }
-
-
 function preecher(bola, a, b, c) {
   let bola_interna = document.getElementById(bola);
   bola_interna.style.backgroundColor = rgb(a, b, c);
@@ -18,7 +16,6 @@ function preecher(bola, a, b, c) {
 function rgb(a, b, c) {
   return 'rgb(' + a + ',' + b + ',' + c + ')'
 }
-
 function startForm() {
   let balas_id = [
     "circulo_1",
@@ -27,7 +24,6 @@ function startForm() {
     "circulo_4",
     "circulo_5",
     "circulo_6"]
-
   for (let i = 0; i < 6; i++) {
     let rand = this.Math.floor(this.Math.random() * (6 - i))
     preecher(balas_id[rand], cor(), cor(), cor());
@@ -36,27 +32,29 @@ function startForm() {
   }
   gerar_RGB();
 }
+//
 startForm();
-let pararRodada=false
+let pararRodada = false
+//
 const quadro = document.getElementById('quadro-cores');
 quadro.addEventListener('click', function (e) {
   let placar = parseInt(document.getElementById('score').innerText, 10);
   const corCerta = 'rgb' + document.getElementById('rgb-color').innerText;
-  if (e.target.className == 'ball'&&pararRodada==false) {
+  if (e.target.className == 'ball' && pararRodada == false) {
     if (e.target.style.backgroundColor == corCerta) {
-      pararRodada=true;
+      pararRodada = true;
       document.getElementById('answer').innerHTML = 'Acertou!';
-      placar+=3;
-      document.getElementById('score').innerText=placar;
+      placar += 3;
+      document.getElementById('score').innerText = placar;
     } else {
       document.getElementById('answer').innerHTML = 'Errou! Tente novamente!';
     }
   }
 });
-
-const btReiniciar = document.getElementById('botao-reiniciar');
+//
+const btReiniciar = document.getElementById('reset-game');
 btReiniciar.addEventListener('click', function () {
   startForm();
   document.getElementById('answer').innerHTML = 'Escolha uma cor';
-  pararRodada=false;
+  pararRodada = false;
 });
